d48390196442f39ccb756825e923a7db59d8d0d7
Felipe Jung <felipemattoseu@gmail.com>
2024-07-16 12:37:48 -0400

    Update way to increment like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -14,7 +14,7 @@ export function Comment({ content, onDeleteComment }) {
   }
 
   function handleLikeComment() {
-    setLikeCount(likeCount + 1);
+    setLikeCount((state) => state + 1);
   }
 
   return (
